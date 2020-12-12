ffa853e3947d5a2640117df0e95bc36c3f3cda7d
Lovro Markovic <lovro.markovic@fer.hr>
2020-12-12 21:46:38 +0100

    Day12 - part2 failed

--- day12.cpp
+++ day12.cpp
@@ -1,14 +1,16 @@
 #include "util.hpp"
+#include <cmath>
 #include <functional>
 #include <unordered_map>
-#include <cmath>
 
 struct ship {
   enum heading_t { EAST = 'E', WEST = 'W', NORTH = 'N', SOUTH = 'S' };
 
   heading_t curr_heading = EAST;
   std::array<int, 2> position{0, 0};
+  std::array<int, 2> waypoint{10, 1};
   std::unordered_map<char, std::function<void(int)>> function_map;
+  std::unordered_map<char, std::function<void(int)>> waypoint_fmap;
 
   heading_t turn_heading(bool positive = true) {
     switch (curr_heading) {
@@ -51,10 +53,14 @@ struct ship {
   }
 
   ship() {
-    function_map.emplace('E', [&](const auto &command) { position[0] += command; });
-    function_map.emplace('W', [&](const auto &command) { position[0] -= command; });
-    function_map.emplace('N', [&](const auto &commnad) { position[1] += commnad; });
-    function_map.emplace('S', [&](const auto &commnad) { position[1] -= commnad; });
+    function_map.emplace('E',
+                         [&](const auto &command) { position[0] += command; });
+    function_map.emplace('W',
+                         [&](const auto &command) { position[0] -= command; });
+    function_map.emplace('N',
+                         [&](const auto &commnad) { position[1] += commnad; });
+    function_map.emplace('S',
+                         [&](const auto &commnad) { position[1] -= commnad; });
     function_map.emplace('F', [&](const auto &command) {
       switch (curr_heading) {
 
@@ -93,6 +99,37 @@ struct ship {
         turn_rate--;
       }
     });
+
+    ////////////////////////////////////////////////////////
+
+    waypoint_fmap.emplace('E',
+                          [&](const auto &command) { waypoint[0] += command; });
+    waypoint_fmap.emplace('W',
+                          [&](const auto &command) { waypoint[0] -= command; });
+    waypoint_fmap.emplace('N',
+                          [&](const auto &commnad) { waypoint[1] += commnad; });
+    waypoint_fmap.emplace('S',
+                          [&](const auto &commnad) { waypoint[1] -= commnad; });
+    waypoint_fmap.emplace('F', [&](const auto &command) {
+      position[0] += command * waypoint[0];
+      position[1] += command * waypoint[1];
+    });
+    waypoint_fmap.emplace('R', [&](const auto &command) {
+      auto copy_waypoint = waypoint;
+      const auto cos_command = command / 90 == 1 ? 0 : -1;
+      const auto sin_command = command / 90 == 1 ? 1 : 0;
+      waypoint[0] =
+          cos_command * copy_waypoint[0] + sin_command * copy_waypoint[1];
+      waypoint[1] =
+          -sin_command * copy_waypoint[0] + cos_command * copy_waypoint[1];
+    });
+    waypoint_fmap.emplace('L', [&](const auto &command) {
+      auto copy_waypoint = waypoint;
+      const auto cos_command = command / 90 == 1 ? 0 : -1;
+      const auto sin_command = command / 90 == 1 ? -1 : 0;
+      waypoint[0] = cos_command * copy_waypoint[0] + sin_command * copy_waypoint[1];
+      waypoint[1] = - sin_command * copy_waypoint[0] + cos_command * copy_waypoint[1];
+    });
   }
 
   void apply_command(const std::string &command) {
@@ -102,14 +139,33 @@ struct ship {
     function_map[c](val);
     // std::cout << "[" << position[0] << ", " << position[1] << "]\n";
   }
+
+  void apply_waypoint_command(const std::string &command) {
+    auto c = command.at(0);
+    auto val = std::stoi(command.substr(1, command.size() - 1));
+    waypoint_fmap[c](val);
+    // std::cout << c << " " << val << "\n";
+    // std::cout << "Ship [" << position[0] << ", " << position[1] << "]\n";
+    // std::cout << "Waypoint [" << waypoint[0] << ", " << waypoint[1] << "]\n\n";
+    // std::string b;
+    // getline(std::cin, b);
+  }
 };
 
 int part1(const std::string &input) {
   auto lines = aoc_util::get_lines(input);
   ship s;
-  for (auto line : lines)
-  {
+  for (const auto &line : lines) {
     s.apply_command(line);
+  }
+  return abs(s.position[0]) + abs(s.position[1]);
+}
+
+int part2(const std::string &input) {
+  auto lines = aoc_util::get_lines(input);
+  ship s;
+  for (const auto &line : lines) {
+    s.apply_waypoint_command(line);
   }
   return abs(s.position[0]) + abs(s.position[1]);
 }
@@ -117,5 +173,7 @@ int part1(const std::string &input) {
 int main() {
   std::cout << "[part1] test solution: " << part1("input12_1.txt") << "\n";
   std::cout << "[part1] solution: " << part1("input12_2.txt") << "\n";
+  std::cout << "[part2] test solution: " << part2("input12_1.txt") << "\n";
+  std::cout << "[part2] solution: " << part2("input12_2.txt") << "\n";
   return 0;
 }
