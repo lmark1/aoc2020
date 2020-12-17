baec8b07ba1def19bc303214296bf35053b2f7b0
Lovro Markovic <lovro.markovic@fer.hr>
2020-12-17 01:09:54 +0100

    Day16 - part2

--- day16.cpp
+++ day16.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
 #include <numeric>
+#include <set>
 #include <unordered_map>
 
 struct range {
@@ -9,9 +10,15 @@ struct range {
   int upper_1;
   int lower_2;
   int upper_2;
+  std::set<int> valid_idxs;
 
   range(int l1, int u1, int l2, int u2)
       : lower_1(l1), upper_1(u1), lower_2(l2), upper_2(u2) {}
+};
+
+struct verification {
+  long long number;
+  bool valid;
 };
 
 using field_map_t = std::unordered_map<std::string, range>;
@@ -68,52 +75,126 @@ parse_input(const std::vector<std::string> &lines) {
   return {field_map, my_ticket, other_tickets};
 }
 
-long long verify_number(int ticket_number, const field_map_t &field_map) {
-  for (const auto &field : field_map) {
-    auto range = field.second;
-    if ((ticket_number >= range.lower_1 && ticket_number <= range.upper_1) ||
-        (ticket_number >= range.lower_2 && ticket_number <= range.upper_2)) {
-      return 0L;
-    }
-  }
-  return static_cast<long long>(ticket_number);
+bool is_in_range(int el, const range &range) {
+  return (el >= range.lower_1 && el <= range.upper_1) ||
+         (el >= range.lower_2 && el <= range.upper_2);
 }
 
-long long verify_ticket(const field_map_t &field_map,
-                        const std::vector<int> &ticket) {
-  long long sum = 0LL;
-  for (const auto &ticket_number : ticket) {
-    sum += verify_number(ticket_number, field_map);
+verification verify_number(int ticket_number, const field_map_t &field_map) {
+  for (const auto &field : field_map) {
+    auto range = field.second;
+    if (is_in_range(ticket_number, range)) {
+      return {0L, true};
+    }
   }
-  return sum;
+  return {static_cast<long long>(ticket_number), false};
+}
+
+verification verify_ticket(const field_map_t &field_map,
+                           const std::vector<int> &ticket) {
+  long long sum = 0LL;
+  bool check = true;
+  for (const auto &ticket_number : ticket) {
+    auto res = verify_number(ticket_number, field_map);
+    check &= res.valid;
+    sum += res.number;
+  }
+  return {sum, check};
+}
+
+void find_corresponding_field(const std::vector<int> &col,
+                              field_map_t &field_map, std::size_t col_idx) {
+  for (auto &field : field_map) {
+    auto &range = field.second;
+    auto check = std::all_of(col.begin(), col.end(), [&](const auto &el) {
+      return is_in_range(el, range);
+    });
+
+    if (check) {
+      range.valid_idxs.insert(col_idx);
+    }
+  }
+}
+
+void filter_valid_indices(field_map_t &field_map, int index_count) {
+  std::set<int> singles;
+  int singles_count = 0;
+  auto in_singles = [&](const auto &el) {
+    return std::binary_search(singles.begin(), singles.end(), el);
+  };
+  while (singles_count < index_count) {
+
+    singles_count = 0;
+    for (auto &field : field_map) {
+      auto &valid_indices = field.second.valid_idxs;
+
+      // if there is more do a set difference and save it
+      if (valid_indices.size() > 1) {
+        std::set<int> diff;
+        std::set_difference(valid_indices.begin(), valid_indices.end(),
+                            singles.begin(), singles.end(),
+                            std::inserter(diff, diff.begin()));
+        valid_indices.swap(diff);
+      }
+
+      // If there is only 1 add it to singles
+      if (valid_indices.size() == 1) {
+        singles.insert(*valid_indices.begin());
+        singles_count++;
+      }
+    }
+  }
 }
 
 long long part1(const std::string &input) {
   auto lines = aoc_util::get_lines(input);
   auto [field_map, my_ticket, other_tickets] = parse_input(lines);
-  return std::accumulate(other_tickets.begin(), other_tickets.end(), 0LL,
-                         [&](long long sum, const auto &ticket) {
-                           return sum + verify_ticket(field_map, ticket);
-                         });
+  return std::accumulate(
+      other_tickets.begin(), other_tickets.end(), 0LL,
+      [&, field_map = std::ref(field_map)](long long sum, const auto &ticket) {
+        return sum + verify_ticket(field_map.get(), ticket).number;
+      });
 }
 
-long long part2(const std::string& input) {
+long long part2(const std::string &input) {
   auto lines = aoc_util::get_lines(input);
   auto [field_map, my_ticket, other_tickets] = parse_input(lines);
 
   // Remove invalid elements
   aoc_util::matrix<int> valid_tickets;
-  std::copy_if(other_tickets.begin(), other_tickets.end(), std::back_inserter(valid_tickets), [&](const auto& el) {
-    return verify_ticket(field_map, el) == 0LL;
-  });
+  std::copy_if(other_tickets.begin(), other_tickets.end(),
+               std::back_inserter(valid_tickets),
+               [&, field_map = std::ref(field_map)](const auto &el) {
+                 return verify_ticket(field_map.get(), el).valid;
+               });
 
+  const auto HEIGHT = valid_tickets.size();
+  const auto WIDTH = valid_tickets.front().size();
+  for (std::size_t i = 0; i < WIDTH; i++) {
+    std::vector<int> column_vals;
+    for (std::size_t j = 0; j < HEIGHT; j++) {
+      column_vals.push_back(valid_tickets[j][i]);
+    }
+    find_corresponding_field(column_vals, field_map, i);
+  }
 
-  return 0LL;
+  filter_valid_indices(field_map, WIDTH);
+  return std::accumulate(
+      field_map.begin(), field_map.end(), 1LL,
+      [&, my_ticket = std::ref(my_ticket)](long long mul, const auto &field) {
+        if (field.first.rfind("departure", 0) != 0) {
+          return mul;
+        }
+        int idx = *field.second.valid_idxs.begin();
+        return mul * my_ticket.get().at(idx);
+      });
 }
 
 // clang-format off
 int main() {
   std::cout << "[part1] test solution: " << part1("input/input16_1.txt") << "\n";
   std::cout << "[part1] solution: " << part1("input/input16_2.txt") << "\n";
+  std::cout << "[part2] test solution: " << part2("input/input16_3.txt") << "\n"; 
+  std::cout << "[part2] test solution: " << part2("input/input16_2.txt") << "\n"; 
   return 0;
 }
